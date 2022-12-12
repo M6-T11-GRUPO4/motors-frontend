af4c392c349964c38c685c8cb90c524a29dd1d8e
Gabriel Pereira <ggabriel.p2003@gmail.com>
2022-12-12 17:19:12 -0300

    feat/add inital structure

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div className="-bg-grey-6">
+      <header className="App-header font ">
+        <p className="font-lexend">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p className="font-inter">Hello word</p>
+        <a
+          className="font"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
