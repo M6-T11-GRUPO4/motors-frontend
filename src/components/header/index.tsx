599ffd51a3480528fe0c4fa3f67846801aaa6907
Gabriel Pereira <ggabriel.p2003@gmail.com>
2022-12-20 14:04:33 -0300

    feat/adicionando componentes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -12,9 +12,10 @@ interface IUserProp {
 }
 
 export const Header = ({ user }: IUserProp) => {
-  const [bool, setBool] = useState(false);
+  const [boolMobile, setBoolMobile] = useState(false);
+  const [boolPerfile, setBoolPerfile] = useState(false);
   const logged = true;
-  console.log(bool);
+  console.log(boolMobile);
 
   return (
     <header className="flex justify-between h-20">
@@ -25,20 +26,20 @@ export const Header = ({ user }: IUserProp) => {
       <div className="flex items-center pr-4 sm:hidden">
         <div className="relative ">
           <button
-            className=" bg-white px-3 py-1"
-            onClick={() => setBool(!bool)}
+            className=" px-3 py-1"
+            onClick={() => setBoolMobile(!boolMobile)}
           >
             <img src={riscos} alt="img" className="w-5 h-5" />
           </button>
           <nav
             className={
-              bool
+              boolMobile
                 ? "static hidden "
-                : "absolute -right-4 top-12 h-56 -bg-grey-10 w-screen space-y-6"
+                : "absolute -right-4 top-12 h-64 w-screen space-y-6 -bg-grey-9"
             }
           >
-            {/* <nav className="absoluteNave  md:flex flex-col md:flex-row bg-white"> */}
-            <ul className="flex flex-col items-start px-8 space-y-3 font-inter">
+            {/* <nav className="absoluteNave  md:flex flex-col md:flex-row -bg-grey-6"> */}
+            <ul className="flex flex-col items-start px-8 space-y-8 mt-10 font-inter">
               <li>
                 <button className={"block w-full hover:-text-brand1"}>
                   Carros
@@ -56,18 +57,47 @@ export const Header = ({ user }: IUserProp) => {
               </li>
             </ul>
             {logged ? (
-              <div className="flex gap-x-2 items-center justify-center">
+              <div
+                className="relative flex gap-x-2 items-center justify-center pr-4"
+                onClick={() => {setBoolPerfile(!boolPerfile)}}
+              >
                 <div className="flex items-center justify-center -bg-brand1 rounded-2xl text-white w-8 h-8 ">
-                  GP
+                  {"GP"}
                 </div>
-                <p>Gabriel Pereira</p>
+                <p>{"Gabriel Pereira"}</p>
+                <ul
+                  className={
+                    boolPerfile
+                      ? "absolute top-14 w-44 flex flex-col space-y-4 pl-2 h-32 "
+                      : "static hidden"
+                  }
+                >
+                  <li>
+                    <button className="hover:-text-brand1">Editar Perfil</button>
+                  </li>
+                  <li>
+                    <button className="hover:-text-brand1">Editar Endereço</button>
+                  </li>
+                  <li>
+                    <button className="hover:-text-brand1">Minha Compras</button>
+                  </li>
+                  <li>
+                    <button onClick={() => console.log("foii")}>Sair</button>
+                  </li>
+                </ul>
               </div>
             ) : (
-              <div className="flex flex-col md:flex-row space-x-8 items-baseline md:items-center font-lexend pr-16 gap-y-3">
+              <div className="relative flex flex-col md:flex-row space-x-8 items-baseline md:items-center font-lexend pr-16 gap-y-3">
                 <button className="hover:-text-brand1">Fazer Login</button>
                 <button className="hover:-text-brand1 -border-grey-3 border rounded font-bold h-10 md:h-8 w-full md:w-28">
                   Cadastrar
                 </button>
+                <ul className="absolute flex flex-col ">
+                  <li>Editar Perfil</li>
+                  <li>Editar Endereço</li>
+                  <li>Minha Compras</li>
+                  <li>Sair</li>
+                </ul>
               </div>
             )}
           </nav>
@@ -75,19 +105,42 @@ export const Header = ({ user }: IUserProp) => {
       </div>
       <div className="hidden sm:flex items-center space-x-10 font-inter ">
         <nav className="flex space-x-3 text-sm h-5">
-          <button className=" hover:-text-brand1 bg-white">Carros</button>
-          <button className=" hover:-text-brand1 bg-white">Motos</button>
-          <button className=" hover:-text-brand1 bg-white">Leilão</button>
+          <button className=" hover:-text-brand1 ">Carros</button>
+          <button className=" hover:-text-brand1 ">Motos</button>
+          <button className=" hover:-text-brand1 ">Leilão</button>
         </nav>
         {logged ? (
-          <div className="flex gap-x-2 items-center pr-4">
+          <div
+            className="relative flex gap-x-2 items-center pr-9"
+            onClick={() => setBoolMobile(!boolMobile)}
+          >
             <div className="flex items-center justify-center -bg-brand1 rounded-2xl text-white w-8 h-8 ">
               {"GP"}
             </div>
             <p>{"Gabriel Pereira"}</p>
+            <ul
+              className={
+                boolMobile
+                  ? "absolute top-14 w-44 flex flex-col -bg-grey-9 space-y-4 pl-4 py-2 h-40"
+                  : "static hidden"
+              }
+            >
+              <li>
+                <button className="hover:-text-brand1">Editar Perfil</button>
+              </li>
+              <li>
+                <button className="hover:-text-brand1">Editar Endereço</button>
+              </li>
+              <li>
+                <button className="hover:-text-brand1">Minha Compras</button>
+              </li>
+              <li>
+                <button onClick={() => console.log("foii")}>Sair</button>
+              </li>
+            </ul>
           </div>
         ) : (
-          <div className="flex space-x-8 items-center text-sm pr-16">
+          <div className=" flex space-x-8 items-center text-sm pr-16">
             <button className="hover:-text-brand1">Fazer Login</button>
             <button className="hover:-text-brand1 -border-grey-3 border rounded font-bold h-8 w-28">
               Cadastrar
---
+++ src/components/mid/index.tsx
@@ -0,0 +1,21 @@
+
+export const Mid = () => {
+  return (
+    <section className="flex flex-col items-center w-screen h-[34rem] md:h-96 text-center justify-center space-y-16 -bg-brand2 font-inter -text-grey-10 ">
+      <h2 className="text-4xl w-12/12 md:w-7/12 font-bold">
+        Velocidade e experiência em um lugar feito para você
+      </h2>
+      <h4 className="text-sm w-64 md:w-96">
+        Um ambiente feito para você explorar o seu melhor
+      </h4>
+      <div className="flex flex-col sm:flex-row items-center justify-center space-y-5 sm:space-x-5 sm:space-y-0 w-80">
+        <button className=" -border-grey-10 border w-80 sm:w-5/12 h-10 rounded">
+          Carros
+        </button>
+        <button className=" -border-grey-10 border w-80 sm:w-5/12 h-10 rounded">
+          Motos
+        </button>
+      </div>
+    </section>
+  );
+};
