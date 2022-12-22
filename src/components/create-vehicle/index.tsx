4d54c7bb5ab6d492c0aeca44cc1b2bf64ba1c0b6
Bruno Tiberio <quentemente@gmail.com>
2022-12-22 07:20:28 -0300

    feat: funcionalidade botoes venda-leilao

--- src/components/create-vehicle/index.tsx
+++ src/components/create-vehicle/index.tsx
@@ -35,7 +35,6 @@ export default function CreateVehicle() {
       left: "50%",
       right: "auto",
       bottom: "auto",
-      border: "2px solid #000000",
       background: "#fff",
       overflow: "auto",
       WebkitOverflowScrolling: "touch",
@@ -53,31 +52,19 @@ export default function CreateVehicle() {
       onRequestClose={OpenAndCloseModal}
       style={customStyles}
     >
-      <div className="flex flex-row justify-between mx-6 mt-5 mb-[2.375rem]">
+      <div className="flex flex-row justify-between mx-6 mt-5 mb-[0.8rem]">
         <p>Criar anuncio</p>
         <button onClick={OpenAndCloseModal}>
           <img src={x} alt="fechar" />
         </button>
       </div>
       <form>
-        <p className="ml-6">Tipo de anuncio</p>
-        <div>
-          <input
-            type="radio"
-            id="venda"
-            name="is_auction"
-            value="Venda"
-            onClick={setAuctionFalse}
-          />
-          <label htmlFor="venda">Venda</label>
-          <input
-            type="radio"
-            id="leilao"
-            name="is_auction"
-            value="Leilão"
-            onClick={setAuctionTrue}
-          />
-          <label htmlFor="leilao">Leilão</label>
+        <p className="ml-6 my-[0.8rem]">Tipo de anuncio</p>
+        <div className="flex flex-row justify-around">
+          <input type="radio" id="venda" name="is_auction" value="Venda" onClick={setAuctionFalse} className='classInputRadio'/>
+          {!isAuction ? <label htmlFor="venda" className="classLabelInputRadioActivate ml-2">Venda</label> : <label htmlFor="venda" className="classLabelInputRadioDesactivate ml-2">Venda</label>}
+          <input type="radio" id="leilao" name="is_auction" value="Leilão" onClick={setAuctionTrue} className='classInputRadio'/>
+          {!isAuction ? <label htmlFor="leilao" className="classLabelInputRadioDesactivate mr-6">Leilão</label> : isAuction && <label htmlFor="leilao" className="classLabelInputRadioActivate mr-6">Leilão</label>}
         </div>
         <p>Informações do veículo</p>
         <label>Título</label>
