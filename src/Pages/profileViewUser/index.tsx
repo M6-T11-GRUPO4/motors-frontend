11e4159b8d135cacc9902aca9e4864d0c4a0f0ac
Gabriel Pereira <ggabriel.p2003@gmail.com>
2023-01-02 16:41:54 -0300

    fix/corrigindo cards

--- src/Pages/profileViewUser/index.tsx
+++ src/Pages/profileViewUser/index.tsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import Cards from "../../components/cards/vehicles";
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 import { ProfileCard } from "../../components/profileCard";
+import { CounterContext } from "../../Providers/counter";
+import { IProducts } from "../home";
 
 export const ProfileViewUser = () => {
   interface IUser {
@@ -15,14 +18,34 @@ export const ProfileViewUser = () => {
     img: "www.google.com",
     isSeller: false,
   };
+  const { response } = useContext(CounterContext);
 
   return (
     <>
       <Header user={user} />
 
-      <main className="h-full bg-blue-white-gradient flex flex-col">
+      <main className="h-full bg-blue-white-gradient flex flex-col select-none">
         <ProfileCard />
-        <Cards />
+        <div className="flex overflow-x-auto mx-4 flex-col ml-6">
+          <h1 className="py-8 font-bold text-lg font-lexend ml-10">Carros</h1>
+          <div className="flex mx-4" id="carro">
+            {response?.map((products: IProducts) => {
+              if (products.type === "Carro") {
+                return <Cards products={products} />;
+              }
+            })}
+          </div>
+        </div>
+        <div className="flex overflow-x-auto mx-4 flex-col ml-6">
+          <h1 className="py-8 font-bold text-lg font-lexend ml-10">Motos</h1>
+          <div className="flex mx-4" id="moto">
+            {response?.map((products: IProducts) => {
+              if (products.type === "Moto") {
+                return <Cards products={products} />;
+              }
+            })}
+          </div>
+        </div>
       </main>
 
       <Footer />
