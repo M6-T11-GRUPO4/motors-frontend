43327aaa08158ff7b6d78e21a9a610254b892329
Debora Correa <deboragomescorr@gmail.com>
2023-01-03 17:33:52 -0300

    fix: modal adicionado à página e variável condiciona aparecimento de isActive no card

--- src/Pages/profile/index.tsx
+++ src/Pages/profile/index.tsx
@@ -1,23 +1,26 @@
 import { useContext } from "react";
+import Auction from "../../components/cards/auction";
 import Cards from "../../components/cards/vehicles";
+import CreateVehicle from "../../components/create-vehicle";
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 import { ProfileCard } from "../../components/profileCard";
 import { CounterContext } from "../../Providers/counter";
 import { IProducts } from "../home";
 
-export const ProfileViewUser = () => {
-  interface IUser {
-    name: string;
-    img: string;
-    isSeller: boolean;
-  }
+export interface IUser {
+  name: string;
+  img: string;
+  isSeller: boolean;
+}
 
+export const ProfileView = () => {
   const user: IUser = {
     name: "Roberto Ferreira",
     img: "www.google.com",
     isSeller: false,
   };
+
   const { response } = useContext(CounterContext);
 
   return (
@@ -25,13 +28,18 @@ export const ProfileViewUser = () => {
       <Header user={user} />
 
       <main className="h-full bg-blue-white-gradient flex flex-col select-none">
-        <ProfileCard />
+        <ProfileCard user={user} />
+
+        <Auction />
+
         <div className="flex overflow-x-auto mx-4 flex-col ml-6">
           <h1 className="py-8 font-bold text-lg font-lexend ml-10">Carros</h1>
           <div className="flex mx-4" id="carro">
             {response?.map((products: IProducts) => {
               if (products.type === "Carro") {
-                return <Cards products={products} />;
+                return (
+                  <Cards products={products} showIsActive={!user.isSeller} />
+                );
               }
             })}
           </div>
@@ -41,7 +49,7 @@ export const ProfileViewUser = () => {
           <div className="flex mx-4" id="moto">
             {response?.map((products: IProducts) => {
               if (products.type === "Moto") {
-                return <Cards products={products} />;
+                return <Cards products={products} showIsActive={!user.isSeller} />;
               }
             })}
           </div>
@@ -49,6 +57,8 @@ export const ProfileViewUser = () => {
       </main>
 
       <Footer />
+
+      <CreateVehicle />
     </>
   );
 };
